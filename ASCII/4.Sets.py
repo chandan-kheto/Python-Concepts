47231ae6c37a34ae8d9bc70fb02d7d27d4c05006
Chandan Kheto <chandankheto46@gmail.com>
2025-07-08 07:54:02 +0530

    Add files via upload

---
+++ ASCII/4.Sets.py
@@ -0,0 +1,10 @@
+ # Concept: A set stores unique, unordered items.
+# example
+
+numbers = {1, 2, 2,3, 4, 5}
+print(numbers) # Duplicates removed automatically
+numbers.add(6)
+print(numbers)
+print('----------------')
+numbers.remove(6)
+print(numbers)
---
+++ ASCII/5.String.py
@@ -0,0 +1,10 @@
+# Concept:Strings behave like lists for indexing and slicing.
+# example
+name = "CHANDAN KHETO"
+print(name[0])  #op: C
+print(name[-1])  #op: O
+print(name[-2])  #op: T
+print(name[2])  #op: A
+print(name[0:5])  #op: CHAND
+print(name[5:12])  #SLICE: AN KHET
+print(name[5:])  #SLICE: AN KHETO
